3505fff72feffd58c46154633607d97235344141
luizh3nrique <luizh3nrique00@gmail.com>
2023-05-18 21:27:59 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 window.addEventListener('DOMContentLoaded', function () {
     var timerElement = document.getElementById('timer');
+    var countdown = 7 * 24 * 60 * 60;
 
     function updateTimer() {
-        var now = new Date();
-        var days = now.getDay();
-        var hours = now.getHours();
-        var minutes = now.getMinutes();
-        var seconds = now.getSeconds();
-
+        var days = Math.floor(countdown / (24 * 60 * 60));
+        var hours = Math.floor((countdown % (24 * 60 * 60)) / (60 * 60));
+        var minutes = Math.floor((countdown % (60 * 60)) / 60);
+        var seconds = Math.floor(countdown % 60);
 
         var formattedTime =
             ('0' + days).slice(-2) + 'd ' +
@@ -15,9 +14,20 @@ window.addEventListener('DOMContentLoaded', function () {
             ('0' + minutes).slice(-2) + ':' +
             ('0' + seconds).slice(-2);
 
-
         timerElement.textContent = formattedTime;
+
+        if (countdown <= 0) {
+            clearInterval(timerInterval);
+            countdown = 7 * 24 * 60 * 60;
+            setTimeout(function () {
+                timerInterval = setInterval(updateTimer, 1000);
+            }, 1000);
+        }
+
+        countdown--;
     }
 
-    setInterval(updateTimer, 1000); // Atualiza o cronômetro a cada segundo
+    var timerInterval = setInterval(updateTimer, 1000);
+
+    updateTimer();
 });
